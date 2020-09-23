ef1e4d3d2f02c35a918ef45b27c2955cec151560
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2020-09-22 20:09:21 -0500

    Setup project.

---
+++ Project17/Project17Tests/Project17Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Project17Tests.swift
+//  Project17Tests
+//
+//  Created by Álvaro Ávalos Hernández on 22/09/20.
+//
+
+import XCTest
+@testable import Project17
+
+class Project17Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
